7b02dff0b8da1b46d60a0ade76acdfc81b1adb0f
muhammadasimsiddiqui <masimsiddiqui056@gmail.com>
2024-07-31 20:27:31 +0500

    my first commit

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('.nav-hero li a').forEach(link => {
+    link.addEventListener('mouseenter', function(event) {
+      const { left, right } = this.getBoundingClientRect();
+      const x = event.clientX;
+  
+      if (x - left < right - x) {
+        this.classList.add('hover-left');
+        this.classList.remove('hover-right');
+      } else {
+        this.classList.add('hover-right');
+        this.classList.remove('hover-left');
+      }
+    });
+  
+    link.addEventListener('mouseleave', function() {
+      this.classList.remove('hover-left', 'hover-right');
+    });
+  
+    // Add event listener to ensure active state is maintained correctly
+    link.addEventListener('click', function() {
+      document.querySelectorAll('.nav-hero li a').forEach(item => item.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+  
+  
